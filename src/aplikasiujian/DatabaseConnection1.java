77c3861648e64b12f18027538fc66059ab4634e1
Naufal Avrilzal Irawan <naufalavir@gmail.com>
2026-06-28 12:45:17 +0700

    Initialize and add a DBconnect and exam UI protoype

---
+++ src/aplikasiujian/DatabaseConnection1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasiujian;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user
+ */
+public class DatabaseConnection1 {
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/exam_db";
+                String user = "root";
+                String password = ""; 
+                
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Database Connection Failed: " + e.getMessage());
+            }
+        }
+        return connection;
+    }
+}
